d286d4eb5b2dfdc86ec75defd241a9a3967cbf59
NurikN999 <nurkamalnurmukhamed@gmail.com>
2020-12-23 13:03:35 +0500

    Final Project

---
+++ Final project/Store/js/script.js
@@ -0,0 +1,43 @@
+const popupLinks = document.querySelectorAll('.popup_link');
+              const body = document.querySelectorAll('.body');
+              const lockPadding = document.querySelectorAll('.lock-padding');
+
+              let unlock = true;
+
+              const timeout="800";
+
+              if (popupLinks.length > 0) {
+                  for (let index = 0; index < popupLinks.length; index++) {
+                    const popupLink = popupLinks[index];
+                    popupLink.addEventListener("click", function (e){
+                        const popupName = popupLink.getAttribute('href').replace('#', '');
+                        const curentPopup = document.getElementById(popupName);
+                        popupOpen(curentPopup);
+                        e.preventDefault();
+                    });S
+                  }
+              }
+              function popupOpen(curentPopup) {
+                if (curentPopup && unlock) {
+                    const popupActive = document.querySelector('.popup.open');
+                    if (popupActive) {
+                        popupClose(popupActive, false);
+                        } else{
+                            bodyLock();
+                        }
+                        currentPopup.classList.add('open');
+                        currentPopup.addEventListener("click", function (e) {
+                            if (!e.target.closest('.popup_content')) {
+                                popupClose(e.target.closest('.popup'));
+                            }
+                        });
+                        }
+                };
+              function popupClose(popupActive, doUnlock = true){
+                  if(unlock){
+                      popupActive.classList.remove('open');
+                      if (doUnlock) {
+                          bodyUnlock();
+                      }
+                  } 
+              }
